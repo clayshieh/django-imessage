8954a2868713a5bca9a3e3640183837812bd0d80
Clay Shieh <shieh.clay@gmail.com>
2017-12-10 16:41:40 -0800

    refactored filtering message functionality

--- sms/messages/views.py
+++ sms/messages/views.py
@@ -6,7 +6,7 @@ from messages.models import Message, Handle
 
 from datetime import datetime, timedelta
 from pytz import timezone
-import time
+
 
 def index(request, template_name="messages/index.html"):
 
@@ -17,33 +17,26 @@ def index(request, template_name="messages/index.html"):
     return render_to_response(template_name,
             RequestContext(request, context))
 
+
 def messages(request, handle_id, template_name="messages/messages.html"):
 
     handle = Handle.objects.get(pk=handle_id)
 
-    context = {
-        "handle": handle,
-    }
-
-    return render_to_response(template_name,
-            RequestContext(request, context))
-
-def messages_filtered(request, handle_id, filter_day, template_name="messages/messages_filtered.html"):
-    handle = Handle.objects.get(pk=handle_id)
-    app_tz = timezone(settings.TIME_ZONE)
-
-    start_date = datetime.fromtimestamp(int(filter_day)/1000).replace(tzinfo=app_tz)
-    end_date = start_date + timedelta(days=1)
-
     messages = []
-    for message in handle.message_set.all():
-        msg_date = message.date
-        if msg_date < start_date:
-            continue
-        elif msg_date > end_date:
-            break
-        else:
-            messages.append(message)
+    if "filter" in request.GET:
+        app_tz = timezone(settings.TIME_ZONE)
+        start_date = datetime.strptime(request.GET["filter"], "%Y-%m-%d").replace(tzinfo=app_tz)
+        end_date = start_date + timedelta(days=1)
+        for message in handle.message_set.all():
+            msg_date = message.date
+            if msg_date < start_date:
+                continue
+            elif msg_date > end_date:
+                break
+            else:
+                messages.append(message)
+    else:
+        messages = handle.message_set.all()
 
     context = {
         "handle": handle,
@@ -51,3 +44,4 @@ def messages_filtered(request, handle_id, filter_day, template_name="messages/me
     }
 
     return render(request, template_name, context)
+
