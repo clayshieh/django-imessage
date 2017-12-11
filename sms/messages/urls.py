9211d9e0c30c62169199abb70819da45c872f646
Clay Shieh <shieh.clay@gmail.com>
2017-12-10 16:45:37 -0800

    removed extra url endpoint

--- sms/messages/urls.py
+++ sms/messages/urls.py
@@ -3,5 +3,4 @@ from django.conf.urls import patterns, include, url
 urlpatterns = patterns('messages.views',
     url(r'^$', 'index'),
     url(r'^(?P<handle_id>\d+)/$', 'messages'),
-    url(r'^(?P<handle_id>\d+)/(?P<filter_day>\d+)/$', 'messages_filtered'),
 )
