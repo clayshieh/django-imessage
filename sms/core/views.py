bfbffb588cb21826c51d3da14a37ad06a4ee97f8
Clay Shieh <shieh.clay@gmail.com>
2017-12-28 08:44:55 +0800

    used django's hot reloading on code change to implement resync of database selection

--- sms/core/views.py
+++ sms/core/views.py
@@ -10,7 +10,6 @@ def index(request):
 
 def databases(request):
     changed = False
-    different = False
     files = listdir(join(join(settings.PROJECT_ROOT, 'db')))
     dbs = []
     for file in files:
@@ -22,20 +21,23 @@ def databases(request):
     if "select" in request.GET and request.GET["select"]:
         select = request.GET["select"].strip()
         if select != config_db:
-            open(join(join(settings.PROJECT_ROOT, 'db'), "db.conf"), "w").write(select)
-            selected_db = select
-            changed = True
+            with open(join(join(settings.PROJECT_ROOT, 'db'), "db.conf"), "w") as f:
+                f.write(select)
+
+            # Jank way of implementing the syncdb functionality below. Works because django hot reloads.
             # from django.core.management import execute_from_command_line
             # execute_from_command_line("syncdb")
-    else:
-        if config_db != settings.DATABASE_NAME:
-            different = True
+            with open(join(settings.CORE_APP_ROOT, "__init__.py"), "w") as f:
+                f.write("#" + select)
+
+            selected_db = select
+            config_db = select
+            changed = True
 
     context = {
         "databases": dbs,
         "selected": selected_db,
         "config_db": config_db,
-        "changed": changed,
-        "different": different
+        "changed": changed
     }
     return render(request, 'db.html', context)
